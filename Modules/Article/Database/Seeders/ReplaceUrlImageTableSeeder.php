170cff42faf2799232f6237aeabb9b5929aad9e9
alexsandro <alexsandro@oooleader.ru>
2019-08-23 11:11:04 +0300

    update stm

---
+++ Modules/Article/Database/Seeders/ReplaceUrlImageTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Article\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Article\Models\Article;
+
+class ReplaceUrlImageTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $articles = Article::get();
+        $articles->each(function($article) {
+          $article->content = str_replace("http://www.hydronix.ru/", "./storage/", $article->content);
+          $article->save();
+          return $article;
+        });
+
+        // $this->call("OthersTableSeeder");
+    }
+}
