5a6dcbdf11cc14f706483a16e5db4cb311200281
alexsandro <alexsandro@oooleader.ru>
2019-08-27 13:36:22 +0300

    fd

--- Modules/Page/Resources/views/show.blade.php
+++ Modules/Page/Resources/views/show.blade.php
@@ -3,27 +3,7 @@
 @section('title', $page->title)
 
 @section('sidebar')
-  <h1><span>Области применения</span></h1>
-  <div class="information">
-    <ul class="article__tab-action">
-      @foreach($interfs as $interf)
-        <li>
-          <a href="{{$interf->url_key.'.html'}}">
-            @if(count($interf->files)>0)
-              @foreach($interf->files[0]->config as $key=>$conf)
-                @foreach($conf as $key=>$item)
-                  @if($key == "extrasmall")
-                    <img src="/storage/{{$item['filename']}}" class="fleft"/>
-                  @endif
-                @endforeach
-              @endforeach
-            @endif
-            {{$interf->short_title?$interf->short_title:$interf->title}}
-          </a>
-        </li>
-      @endforeach
-    </ul>
-  </div>
+
 @endsection
 
 @section('content')
